ed2cd6319bcff80952f5aa1b72f65b7d01c9bcb1
OriginalAnswer <akiseonair@gmail.com>
2023-08-16 19:31:10 +0900

    .

--- search.js
+++ search.js
@@ -1,10 +1,7 @@
 const originView = document.querySelector('.links');
 const currentView = document.querySelector('.currentview');
-const allLinks = document.querySelectorAll('.link');
+const allLinks = document.querySelectorAll('.link'); 
 function search(v) {
-    console.log(v);
-    console.log(originView);
-    console.log(currentView);
     if (v == "") {
         originView.classList.remove('dpnone')
         currentView.classList.add('dpnone');
@@ -12,5 +9,7 @@ function search(v) {
         originView.classList.add('dpnone');
         currentView.classList.remove('dpnone')
     };
-    let currentLink = document.querySelectorAll('div
+    const n = document.querySelector('.item-name');
+
+    for (var i = 0; i < n.lenght)
 }
